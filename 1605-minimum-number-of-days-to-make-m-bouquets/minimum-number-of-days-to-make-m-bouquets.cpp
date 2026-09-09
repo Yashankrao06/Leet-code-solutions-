f53323cb108051b3e85da4c3f5b93bc164a788ca
Yashank Vardhan Rao Ben <87665328+Yashankrao06@users.noreply.github.com>
2026-09-10 00:02:53 +0530

    Time: 19 ms (80.34%) | Memory: 70.6 MB (12.22%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+private:
+    bool canMakeBouquets(vector<int>& bloomDay, int m, int k, int day) {
+        int bouquets = 0;
+        int flowers = 0;
+        for (int bloom : bloomDay) {
+            if (bloom <= day) {
+                flowers++;
+                if (flowers == k) {
+                    bouquets++;
+                    flowers = 0;
+                }
+            } else {
+                flowers = 0;
+            }
+        }
+        return bouquets >= m;
+    }
+
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        long long totalFlowersNeeded = 1LL * m * k;
+        if (totalFlowersNeeded > bloomDay.size()) {
+            return -1;
+        }
+
+        int left = *min_element(bloomDay.begin(), bloomDay.end());
+        int right = *max_element(bloomDay.begin(), bloomDay.end());
+        int minWaitDays = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (canMakeBouquets(bloomDay, m, k, mid)) {
+                minWaitDays = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return minWaitDays;
+    }
+};
