693888f97e53f19d43cdebccab48b7895d1da657
Yashank Vardhan Rao Ben <87665328+Yashankrao06@users.noreply.github.com>
2026-09-22 22:29:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (77.45%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+     int sum_s = 0, sum_t = 0;
+        
+        for (char c : s) sum_s += c;
+        for (char c : t) sum_t += c;
+        
+        return (char)(sum_t - sum_s);
+    }
+};
