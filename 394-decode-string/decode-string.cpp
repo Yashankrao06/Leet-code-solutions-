7f1c6e505cd9fb80919caef2f64d3d77b13238de
Yashank Vardhan Rao Ben <87665328+Yashankrao06@users.noreply.github.com>
2026-09-25 21:25:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (36.33%) - LeetSync

---
+++ 394-decode-string/decode-string.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> counts;
+        stack<string> result;
+        string currentStr = "";
+        int k = 0;
+        
+        for (char ch : s) {
+            // Digit build karo
+            if (isdigit(ch)) {
+                k = k * 10 + (ch - '0');
+            }
+            // 2. '[' milne par state stack me save karo
+            else if (ch == '[') {
+                counts.push(k);
+                result.push(currentStr);
+                
+                // Reset for the content inside '['
+                k = 0;
+                currentStr = "";
+            }
+            // ']' milne par decode karke expand karo
+            else if (ch == ']') {
+                int count = counts.top(); counts.pop();
+                string prevStr = result.top(); result.pop();
+                
+                // Decode string by repeating currentStr 'count' times
+                string expanded = "";
+                for (int i = 0; i < count; i++) {
+                    expanded += currentStr;
+                }
+                
+                // Combine with previous context
+                currentStr = prevStr + expanded;
+            }
+            //  Normal alphabet character
+            else {
+                currentStr += ch;
+            }
+        }
+        
+        return currentStr;
+    }
+};
